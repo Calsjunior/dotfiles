a6b8d5383c90e057b5ff81f6fde391a6344df37d
cal <sakphea05@gmail.com>
2025-12-19 20:34:40 +0700

    feat(nvim-nightly): add more plugins

--- nvim-nightly/.config/nvim/init.lua
+++ nvim-nightly/.config/nvim/init.lua
@@ -2,7 +2,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 4
 vim.o.statuscolumn = "%s %l %r "
-vim.o.signcolumn = "no"
+vim.o.signcolumn = "yes"
 vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.softtabstop = 3
@@ -10,6 +10,7 @@ vim.o.clipboard = "unnamedplus"
 vim.o.undofile = true
 vim.o.cursorline = true
 vim.o.smartcase = true
+vim.o.ignorecase = true
 vim.o.termguicolors = true
 vim.o.winborder = "rounded"
 
@@ -17,23 +18,36 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.pack.add({
-		{src = "https://github.com/neanias/everforest-nvim"},
-		{src = "https://github.com/ellisonleao/gruvbox.nvim"},
-		{src = "https://github.com/mikavilpas/yazi.nvim"},
-		{src = "https://github.com/MeanderingProgrammer/render-markdown.nvim"},
-		{src = "https://github.com/YousefHadder/markdown-plus.nvim"},
-		{src = "https://github.com/nvim-lualine/lualine.nvim"},
-		{src = "https://github.com/akinsho/bufferline.nvim"},
-		{src = "https://github.com/kdheepak/lazygit.nvim"},
-		{src = "https://github.com/lewis6991/gitsigns.nvim"},
-		{src = "https://github.com/kawre/neotab.nvim"},
-		{src = "https://github.com/kylechui/nvim-surround"},
-		{src = "https://github.com/neovim/nvim-lspconfig"},
-		{src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+	-- Colors
+	{ src = "https://github.com/neanias/everforest-nvim" },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 
-		-- Dependencies
-		{src = "https://github.com/nvim-tree/nvim-web-devicons"},
-		{src = "https://github.com/nvim-lua/plenary.nvim"},
+	-- Markdown
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+	{ src = "https://github.com/YousefHadder/markdown-plus.nvim" },
+
+	-- Beautify
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/akinsho/bufferline.nvim" },
+	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim"},
+
+	-- Editing
+	{ src = "https://github.com/mikavilpas/yazi.nvim" },
+	{ src = "https://github.com/kawre/neotab.nvim" },
+	{ src = "https://github.com/kylechui/nvim-surround" },
+	{ src = "https://github.com/nvim-mini/mini.pairs" },
+
+	-- Git
+	{ src = "https://github.com/kdheepak/lazygit.nvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+
+	-- LSP
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+
+	-- Dependencies
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 })
 
 local map = vim.keymap.set
@@ -44,6 +58,7 @@ map("n", "<leader>o", ":update<CR> :source<CR>")
 map("n", "<C-s>", ":write<CR>")
 map("n", "<leader>qq", ":quit<CR>")
 map("n", "<Esc><Esc>", ":nohlsearch<CR>")
+map("n", "<leader>bd", ":bd<CR>")
 
 -- Movement
 map("n", "j", "gj")
@@ -51,22 +66,26 @@ map("n", "k", "gk")
 map("n", "H", "_")
 map("n", "L", "g$")
 
--- Plugins
+-- Plugins Keymaps
 map("n", "<leader>lf", vim.lsp.buf.format)
-map("n", "<leader>gg", ":LazyGit<CR>")
+map("n", "<leader>m", ":Mason<CR>")
+map("n", "<leader>gg", ":LazyGitCurrentFile<CR>")
 map("n", "<leader>e", ":Yazi<CR>")
 map("n", "<leader>E", ":Yazi cwd<CR>")
 
--- Setup
+-- Plugin Setup
 require("current_theme")
 require("lualine").setup()
 require("bufferline").setup()
+require("ibl").setup()
 require("nvim-surround").setup()
+require("mini.pairs").setup()
 require("neotab").setup()
 require("gitsigns").setup()
+require("mason").setup()
 
--- Configuration
-vim.lsp.enable({"lua_ls", "clangd"})
+-- Lsp Configuration
+vim.lsp.enable({ "lua_ls", "clangd" })
 
 
 -- Autocmd
@@ -74,8 +93,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, cliend.id, ev.buf, { autotrigger = true })
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
 	end,
 })
 vim.cmd("set completeopt+=noselect")
+
+vim.api.nvim_create_autocmd("UIEnter", {
+	callback = function()
+		require("yazi").setup({
+			open_for_directories = true,
+		})
+	end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
