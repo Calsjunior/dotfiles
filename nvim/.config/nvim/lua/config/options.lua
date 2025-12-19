00b064c3f9e933a8a38a6389aef485d4950101ae
cal <sakphea05@gmail.com>
2025-12-19 10:51:45 +0700

    refactor(nvim): clean up options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,28 +1,23 @@
--- Change Leader key
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
-
--- QOL options
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 5
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.smartindent = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.autoread = true
-vim.opt.hidden = true
-vim.opt.undofile = true
-vim.opt.cursorline = true
-vim.opt.guicursor =
-    "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100,i-ci:ver25-Cursor/lCursor,r-cr:hor20-Cursor/lCursor"
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 5
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.smartindent = true
+vim.o.clipboard = "unnamedplus"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.autoread = true
+vim.o.hidden = true
+vim.o.undofile = true
+vim.o.cursorline = true
 
 -- Disable netrw
 vim.g.loaded_netrwPlugin = 1
