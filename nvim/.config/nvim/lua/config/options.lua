dc247dedd9e6090dfb789b1adfb03e64b71ae726
cal <sakphea05@gmail.com>
2026-03-07 17:01:36 +0700

    feat(nvim): add textwidth option

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,6 +5,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.wrap = false
+vim.opt.textwidth = 75
 vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 8
 
--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -3,6 +3,7 @@ return {
     init = function()
         vim.g.loaded_netrwPlugin = 1
     end,
+    event = "VeryLazy",
     opts = {
         open_for_directories = true,
     },
