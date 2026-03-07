85037acab847637618ebd10e083be3e48bcb0a74
cal <sakphea05@gmail.com>
2026-03-07 18:02:57 +0700

    refactor(nvim): lazy load typst preview

--- nvim/.config/nvim/lua/plugins/typst-preview.lua
+++ nvim/.config/nvim/lua/plugins/typst-preview.lua
@@ -2,5 +2,6 @@ return {
     "chomosuke/typst-preview.nvim",
     lazy = false, -- or ft = 'typst'
     version = "1.*",
+    ft = "typst",
     opts = {}, -- lazy.nvim will implicitly calls `setup {}`
 }
