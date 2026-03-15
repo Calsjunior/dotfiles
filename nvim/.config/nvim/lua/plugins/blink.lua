79d9e34739d0ac732a72f450bd04fa9dd07b98fc
cal <sakphea05@gmail.com>
2026-03-15 21:26:43 +0700

    refactor(nvim): use blink latest version as it fixes bugs related to version 1.9

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,13 +1,5 @@
--- blink.cmp v1.9.x introduced a regression where its internal <Esc> handling
--- intercepts the escape character during `norm` commands (e.g. `g/pattern/norm Itext ^[f.r:`),
--- causing subsequent lines to break. The issue exists regardless of whether
--- <Esc> is mapped in the keymap config or not, meaning blink intercepts it
--- internally. Pinned to v1.8.x until this is fixed upstream.
--- Bugged with flash.nvim on version 1.9.x
-
 return {
     "saghen/blink.cmp",
-    version = "1.8.*",
     opts = {
         completion = {
             accept = { auto_brackets = { enabled = false } },
@@ -26,14 +18,6 @@ return {
         keymap = {
             preset = "default",
             ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-            ["<C-CR>"] = { "select_and_accept", "fallback" },
-
-            ["<Esc>"] = {
-                function(cmp)
-                    cmp.cancel()
-                    vim.cmd("stopinsert")
-                end,
-            },
         },
     },
 }
