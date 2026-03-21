ecb01fbb00a7770e7bc713d3be29a582a0b150eb
cal <sakphea05@gmail.com>
2026-03-21 21:19:59 +0700

    feat(nvim): add linting with biome

---
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        opts = {
+            linters_by_ft = {
+                html = { "biomejs" },
+                javascript = { "biomejs" },
+                typescript = { "biomejs" },
+                json = { "biomejs" },
+            },
+        },
+    },
+}
