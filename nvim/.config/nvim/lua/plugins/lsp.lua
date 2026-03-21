0af89cbc0b42ca776f073e1b2608d0b2f845cf1d
cal <sakphea05@gmail.com>
2026-03-21 21:11:46 +0700

    refactor(nvim): remove html formatting from lsp
    Will use biome to format + lint

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -22,19 +22,6 @@ return {
             inlay_hints = { enabled = false },
             folds = { enabled = false },
             servers = {
-                html = {
-                    filetypes = { "html", "htmldjango", "jinja", "jinga2" },
-                    settings = {
-                        html = {
-                            format = {
-                                wrapLineLength = 160,
-                                extraLiners = "",
-                                indentInnerHtml = true,
-                                templating = true,
-                            },
-                        },
-                    },
-                },
                 emmet_language_server = { "html", "css", "javascript", "javascriptreact", "typescriptreact" },
                 docker_language_server = {
                     filetypes = { "yaml" },
