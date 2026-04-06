e1be28fce06cdc77f713adc49f3b2664418a003b
cal <sakphea05@gmail.com>
2026-04-06 16:41:46 +0700

    feat(yazi): use default terminal colors instead of yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,1 +1,25 @@
 require("full-border"):setup()
+
+function Entity:icon()
+    local icon = self._file:icon()
+    if not icon then
+        return ui.Span("")
+    end
+
+    local span = ui.Span(icon.text .. " ")
+
+    local hovered = cx.active.current.hovered
+    if hovered and tostring(hovered.url) == tostring(self._file.url) then
+        return span
+    end
+
+    if self._file.cha.is_dir then
+        return span:fg("blue")
+    end
+
+    if icon.style then
+        return span:style(icon.style)
+    else
+        return span
+    end
+end
