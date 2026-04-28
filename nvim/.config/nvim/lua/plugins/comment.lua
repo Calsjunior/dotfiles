d8fd8b4043a7097c9c0d333e3f9feb0823c2fd79
cal <sakphea05@gmail.com>
2026-04-28 21:27:53 +0700

    feat(nvim): add neogen to generate docs and mini align to align doc comments

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,54 @@
 return {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    opts = {},
+    {
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
+        "danymat/neogen",
+        opts = {
+            enabled = true,
+            languages = {
+                cpp = {
+                    template = {
+                        annotation_convention = "doxygen",
+                    },
+                },
+            },
+        },
+        keys = {
+            {
+                "<leader>cD",
+                function()
+                    require("neogen").generate()
+                end,
+                desc = "Generate annotation",
+            },
+            {
+                "<leader>cH",
+                function()
+                    require("neogen").generate({ type = "file" })
+                end,
+                desc = "Generate File Header",
+            },
+        },
+    },
+    {
+        "nvim-mini/mini.align",
+        opts = function()
+            local align = require("mini.align")
+
+            return {
+                mappings = {
+                    start = "ga",
+                    start_with_preview = "gA",
+                },
+                modifiers = {
+                    ["d"] = function(steps, _)
+                        table.insert(steps.pre_justify, align.gen_step.filter("n <= 4"))
+                    end,
+                },
+            }
+        end,
+    },
 }
