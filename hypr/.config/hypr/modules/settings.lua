914369acd98c46908da421d88325334fd0b7fe11
cal <sakphea05@gmail.com>
2026-05-15 23:15:15 +0700

    feat(hypr): add gesture scrolling through workspace

--- hypr/.config/hypr/modules/settings.lua
+++ hypr/.config/hypr/modules/settings.lua
@@ -72,6 +72,12 @@ hl.config({
     },
 })
 
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace",
+})
+
 hl.device({
     name = "epic-mouse-v1",
     sensitivity = -0.5,
