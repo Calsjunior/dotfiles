944ab42c021d30cb881e188584d899d8dcc1fa61
cal <sakphea05@gmail.com>
2026-06-02 19:14:00 +0700

    feat(font): add font rendering config

--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -12,6 +12,23 @@
   config = lib.mkIf config.sys.fonts.enable {
     fonts.packages = with pkgs; [
       nerd-fonts.jetbrains-mono
+      noto-fonts
+      noto-fonts-color-emoji
     ];
+
+    # Font rendering
+    fonts.fontconfig = {
+      enable = true;
+      antialias = true;
+      hinting = {
+        enable = true;
+        autohint = false;
+        style = "full";
+      };
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "light";
+      };
+    };
   };
 }
