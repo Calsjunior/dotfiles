b2e2c166d932b4ef5b214ea514e4ab49e46d693a
cal <sakphea05@gmail.com>
2026-06-05 20:03:22 +0700

    fix(yazi): make dir icon colors more vibrant

--- modules/home-manager/cli/yazi.nix
+++ modules/home-manager/cli/yazi.nix
@@ -36,6 +36,31 @@ in
 
       initLua = ''
         require("full-border"):setup()
+
+        -- Fix directories blue color icon
+        function Entity:icon()
+          local icon = self._file:icon()
+            if not icon then
+                return ui.Span("")
+            end
+
+            local span = ui.Span(icon.text .. " ")
+
+            local hovered = cx.active.current.hovered
+            if hovered and tostring(hovered.url) == tostring(self._file.url) then
+                return span
+            end
+
+            if self._file.cha.is_dir then
+                return span:fg("blue")
+            end
+
+            if icon.style then
+                return span:style(icon.style)
+            else
+                return span
+            end
+        end
       '';
 
       plugins = {
