b2e1cd4dbd8b6c2bacea3bad88cc1ae3fab12e27
cal <sakphea05@gmail.com>
2026-06-05 11:46:54 +0700

    feat(hypr): add and autostart noctalia

--- config/hypr/modules/programs.lua
+++ config/hypr/modules/programs.lua
@@ -4,6 +4,7 @@ return {
     terminal = terminal,
     fileManager = terminal .. " yazi",
     browser = "zen-beta",
+    desktopShell = "noctalia",
     editor = terminal .. " nvim",
     session = [[kitty -1 -e zsh -c "nvim +\"lua vim.schedule(function() vim.api.nvim_input('s') end)\""]],
     scripts = os.getenv("HOME") .. "/.config/hypr/scripts",
