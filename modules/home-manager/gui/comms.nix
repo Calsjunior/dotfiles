30c0524940d1f5208168135617d9c9bade06eacd
cal <sakphea05@gmail.com>
2026-06-06 10:02:44 +0700

    feat(comms): add telegram

---
+++ modules/home-manager/gui/comms.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options = {
+    gui.comms.enable = lib.mkEnableOption "Enable Communication Apps";
+  };
+
+  config = lib.mkIf config.gui.comms.enable {
+
+    home.packages = with pkgs; [
+      telegram-desktop
+    ];
+
+  };
+}
