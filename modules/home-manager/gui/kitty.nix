f6862427ffed5afef2ef6ecd6efc3324639aa55d
cal <sakphea05@gmail.com>
2026-06-07 14:45:16 +0700

    feat(kitty): use sensible keymaps

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
@@ -12,36 +11,32 @@
   config = lib.mkIf config.gui.kitty.enable {
     programs.kitty = {
       enable = true;
-
       font = {
         name = "JetBrainsMono Nerd Font";
         size = 14;
       };
-
       themeFile = "GruvboxMaterialDarkMedium";
 
       settings = {
-        kitty_mod = "ctrl+shift";
+        clear_all_shortcuts = "yes";
+        map_timeout = "1.5";
 
-        # Appearance
+        # Appearance & Layouts
         cursor_shape = "block";
         cursor_trail = 3;
         cursor_trail_decay = "0.1 0.4";
         shell_integration = "no-cursor";
-
         tab_bar_edge = "bottom";
         tab_bar_style = "powerline";
         tab_powerline_style = "slanted";
         tab_title_template = " {index}: {title[title.rfind('/')+1:]} ";
-
         touch_scroll_multiplier = 8;
         window_padding_width = "0 5";
-
-        # Layouts
         enabled_layouts = "tall,splits,stack";
 
-        # Window & Session Behavior
+        # Window Behavior
         allow_remote_control = "yes";
+        listen_on = "unix:/tmp/mykitty"; # Required for neovim integration later!
         remember_window_size = "yes";
         initial_window_width = 1000;
         initial_window_height = 650;
@@ -56,31 +51,47 @@
       };
 
       keybindings = {
+        # Window Management
+        "alt+w>c" = "new_window_with_cwd";
+        "alt+w>q" = "close_window";
+
+        "alt+w>v" = "launch --location=vsplit --cwd=current";
+        "alt+w>s" = "launch --location=hsplit --cwd=current";
+
+        "alt+w>h" = "neighboring_window left";
+        "alt+w>l" = "neighboring_window right";
+        "alt+w>k" = "neighboring_window up";
+        "alt+w>j" = "neighboring_window down";
+
+        # Tab Management
+        "alt+tab>tab" = "new_tab_with_cwd";
+        "alt+tab>]" = "next_tab";
+        "alt+tab>[" = "previous_tab";
+        "alt+tab>d" = "close_tab";
+
+        # Layouts
+        "alt+w>m" = "toggle_layout tall";
+        "alt+w>z" = "toggle_layout stack"; # Zoom current window to full screen
+
         # Scrolling
         "alt+u" = "scroll_page_up";
         "alt+d" = "scroll_page_down";
-        "alt+shift+k" = "scroll_line_up";
-        "alt+shift+j" = "scroll_line_down";
+        "alt+k" = "scroll_line_up";
+        "alt+j" = "scroll_line_down";
 
-        # Windows
-        "alt+enter" = "new_window_with_cwd";
-        "alt+q" = "close_window";
-        "alt+r" = "start_resizing_window";
-        "alt+h" = "neighboring_window left";
-        "alt+l" = "neighboring_window right";
-        "alt+k" = "neighboring_window up";
-        "alt+j" = "neighboring_window down";
+        "alt+1" = "goto_tab 1";
+        "alt+2" = "goto_tab 2";
+        "alt+3" = "goto_tab 3";
+        "alt+4" = "goto_tab 4";
 
-        # Tabs
-        "kitty_mod+t" = "new_tab_with_cwd";
-        "kitty_mod+l" = "next_tab";
-        "kitty_mod+h" = "previous_tab";
-        "kitty_mod+," = "move_tab_backward";
-        "kitty_mod+." = "move_tab_forward";
+        # Font size
+        "ctrl+equal" = "change_font_size all +1.0";
+        "ctrl+minus" = "change_font_size all -1.0";
+        "ctrl+0" = "change_font_size all 0";
 
-        # Layouts
-        "kitty_mod+m" = "toggle_layout tall";
-        "kitty_mod+space" = "swap_with_window";
+        # Copy and paste
+        "ctrl+shift+c" = "copy_to_clipboard";
+        "ctrl+shift+v" = "paste_from_clipboard";
       };
     };
 
