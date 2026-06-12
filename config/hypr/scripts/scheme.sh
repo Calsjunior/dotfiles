c9632823806786d7c824e8031ed360c2b594c013
cal <sakphea05@gmail.com>
2026-06-12 18:20:45 +0700

    refactor(hypr): remove unused scripts

--- config/hypr/scripts/scheme.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-caelestia shell drawers toggle launcher &
-sleep 0.41
-wtype ">scheme"
-sleep 0.4
-wtype -k KP_Enter
--- config/hypr/scripts/wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-caelestia shell drawers toggle launcher &
-sleep 0.41
-wtype ">wallpaper"
-sleep 0.4
-wtype -k KP_Enter
