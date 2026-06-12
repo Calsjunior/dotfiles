9a20c75fa381fe5164d1aa47c1440b5d1d130268
cal <sakphea05@gmail.com>
2026-06-12 11:47:23 +0700

    refactor(neovim): move mini align to mini.lua

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -33,22 +33,4 @@ return {
             },
         },
     },
-    {
-        "nvim-mini/mini.align",
-        opts = function()
-            local align = require("mini.align")
-
-            return {
-                mappings = {
-                    start = "ga",
-                    start_with_preview = "gA",
-                },
-                modifiers = {
-                    ["d"] = function(steps, _)
-                        table.insert(steps.pre_justify, align.gen_step.filter("n <= 4"))
-                    end,
-                },
-            }
-        end,
-    },
 }
