fdbf289d0fa78800eeb0ce0cde3f5cdfd0418e0b
cal <sakphea05@gmail.com>
2026-06-14 23:55:47 +0700

    feat(noctalia,hypr): apply persistence workspace

--- config/hypr/modules/monitors.lua
+++ config/hypr/modules/monitors.lua
@@ -1,20 +1,26 @@
-hl.monitor({
-    output = "eDP-1",
-    mode = "1920x1080@60",
-    position = "0x0",
-    scale = "1",
-})
+local monitors = {
+	internal = {
+		output = "eDP-1",
+		mode = "1920x1080@60",
+		position = "0x0",
+		scale = "1",
+	},
+	external1 = {
+		output = "DP-3",
+		mode = "1920x1080@165",
+		position = "0x0",
+		scale = "1",
+	},
+	external2 = {
+		output = "DP-1",
+		mode = "1920x1080@165",
+		position = "0x0",
+		scale = "1",
+	},
+}
 
-hl.monitor({
-    output = "DP-3",
-    mode = "1920x1080@165",
-    position = "0x0",
-    scale = "1",
-})
+for _, m in pairs(monitors) do
+	hl.monitor(m)
+end
 
-hl.monitor({
-    output = "DP-1",
-    mode = "1920x1080@165",
-    position = "0x0",
-    scale = "1",
-})
+return monitors
