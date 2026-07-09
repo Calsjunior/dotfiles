2c7b7f0c0e6d6f472b3ce19538093f64267b5264
cal <sakphea05@gmail.com>
2026-07-09 17:39:40 +0700

    feat(neovim): replace render-markdown with markview plugin

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,16 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
+    "OXY2DEV/markview.nvim",
     ft = "markdown",
+    opts = {
+      preview = {
+        enable = false,
+      },
+    },
+    keys = {
+      { "<leader>mi", "<cmd>Markview toggle<cr>", desc = "Toggle Markview (Inline)" },
+      { "<leader>mv", "<cmd>Markview splitToggle<cr>", desc = "Toggle Markview (Split)" },
+    },
   },
   {
     "yousefhadder/markdown-plus.nvim",
