fff2daf32c94ad618189e5a8ff7156ceac212f10
cal <sakphea05@gmail.com>
2026-08-10 01:32:57 +0700

    refactor(hypr): remove unused cliphist daemon

--- config/hypr/modules/autostart.lua
+++ config/hypr/modules/autostart.lua
@@ -4,8 +4,4 @@ hl.on("hyprland.start", function()
   hl.exec_cmd("sleep 1 && " .. apps.terminal .. " --start-as=hidden")
 
   hl.exec_cmd(apps.desktopShell)
-
-  -- Clipboard Manager
-  hl.exec_cmd("wl-paste --type text --watch cliphist store")
-  hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
