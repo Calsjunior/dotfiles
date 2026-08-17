d3904ba3c8b1e5dca90762d12ba834d7adbdea2e
cal <sakphea05@gmail.com>
2026-08-18 01:16:26 +0700

    feat(neovim): override the default keymap for jupynvim plugin

--- config/nvim/lua/plugins/tools.lua
+++ config/nvim/lua/plugins/tools.lua
@@ -29,6 +29,41 @@ return {
         log_level = "info",
         image_renderer = "kitty",
         core_path = vim.fn.stdpath("data") .. "/lazy/jupynvim/core/target/release/jupynvim-core",
+
+        -- Override the default <leader>n mappings to <leader>j
+        keymaps = {
+          run_advance = "<leader>jr",
+          run_all = "<leader>jR",
+          run_above = "<leader>jA",
+          run_below = "<leader>jB",
+
+          add_above = "<leader>ja",
+          add_below = "<leader>jb",
+          delete_cell = "<leader>jd",
+          move_up = "<leader>jk",
+          move_down = "<leader>jj",
+          to_markdown = "<leader>jm",
+          to_code = "<leader>jy",
+          clear_output = "<leader>jc",
+          clear_all = "<leader>jC",
+
+          save_image = "<leader>jI",
+          delete_image = "<leader>jD",
+
+          pick_kernel = "<leader>jK",
+          start_kernel = "<leader>js",
+          stop_kernel = "<leader>jS",
+          interrupt_kernel = "<leader>ji",
+          restart_kernel = "<leader>jx",
+          refresh = "<leader>jL",
+
+          run_stay = false,
+          run_advance_alt = false,
+          next_cell = false,
+          prev_cell = false,
+          next_image = false,
+          prev_image = false,
+        },
       })
     end,
   },
