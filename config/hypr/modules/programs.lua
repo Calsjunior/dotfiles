6b9c6b5fcd2f1612efb1502deddcc0f9a0e99bb7
cal <sakphea05@gmail.com>
2026-09-08 23:21:14 +0700

    refactor(hypr): update session program to use mini.sessions

--- config/hypr/modules/programs.lua
+++ config/hypr/modules/programs.lua
@@ -7,7 +7,7 @@ return {
   browser = "zen-beta",
   desktopShell = "noctalia",
   editor = terminal .. " nvim",
-  session = [[kitty -1 -e zsh -c "nvim +\"lua vim.schedule(function() vim.api.nvim_input('s') end)\""]],
+  session = [[kitty -1 -e zsh -c "nvim +\"lua vim.schedule(function() require('mini.sessions').select() end)\""]],
   scripts = os.getenv("HOME") .. "/.config/hypr/scripts",
   ipc = ipc,
 }
