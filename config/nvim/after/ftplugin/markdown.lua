1ec5f129f743b54b47a1d8904210003ccafc907d
cal <sakphea05@gmail.com>
2026-09-11 11:33:18 +0700

    feat(neovim)!: remove snacks image

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,2 @@
 vim.opt_local.textwidth = 80
 vim.opt_local.conceallevel = 0
-
-local ok, image_doc = pcall(require, "snacks.image.doc")
-if ok then
-  image_doc.attach(vim.api.nvim_get_current_buf())
-end
