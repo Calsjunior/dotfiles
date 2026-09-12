408c7a331668edcf5d74652c9c5cb96e706af90c
cal <sakphea05@gmail.com>
2026-09-12 17:01:43 +0700

    feat(neovim): pairs math symbol for typst file

--- config/nvim/after/ftplugin/typst.lua
+++ config/nvim/after/ftplugin/typst.lua
@@ -1,1 +1,7 @@
 vim.opt_local.textwidth = 80
+
+-- Pairs math symbol
+local ok, pairs = pcall(require, "mini.pairs")
+if ok then
+  pairs.map_buf(0, "i", "$", { action = "closeopen", pair = "$$" })
+end
